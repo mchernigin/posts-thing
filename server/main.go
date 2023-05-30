8cf3a583a5c9740719fe40e17409335e0fdf14c3
Michael Chernigin <michaelchernigin@gmail.com>
2023-05-31 03:31:12 +0400

    Routes as package

--- server/main.go
+++ server/main.go
@@ -1,161 +1,16 @@
 package server
 
 import (
-	"log"
-	"net/http"
-	"strconv"
-	"time"
-
 	"github.com/gin-gonic/gin"
 	"github.com/jmoiron/sqlx"
-	"github.com/mchernigin/posts-thing/database"
+	"github.com/mchernigin/posts-thing/server/routes"
 )
-
-func getAllPosts(db *sqlx.DB) gin.HandlerFunc {
-	return func(c *gin.Context) {
-		posts, err := database.GetAllPosts(db)
-		if err != nil {
-			log.Fatalln(err)
-		}
-		c.JSON(http.StatusOK, posts)
-	}
-}
-
-func getPost(db *sqlx.DB) gin.HandlerFunc {
-	return func(c *gin.Context) {
-		idStr := c.Param("id")
-		id, err := strconv.ParseInt(idStr, 10, 64)
-		if err != nil {
-			c.JSON(http.StatusBadRequest, "id is not a number")
-			return
-		}
-		post, err := database.GetPostById(db, id)
-		if err != nil {
-			c.JSON(http.StatusNotFound, "not found")
-			return
-		}
-		c.JSON(http.StatusOK, post)
-	}
-}
-
-func deletePost(db *sqlx.DB) gin.HandlerFunc {
-	return func(c *gin.Context) {
-		idStr := c.Param("id")
-		id, err := strconv.ParseInt(idStr, 10, 64)
-		if err != nil {
-			c.JSON(http.StatusBadRequest, "id is not a number")
-			return
-		}
-		err = database.DeletePostById(db, id)
-		if err != nil {
-			c.JSON(http.StatusNotFound, "not found")
-			return
-		}
-		c.Status(http.StatusOK)
-	}
-}
-
-func deleteAuthor(db *sqlx.DB) gin.HandlerFunc {
-	return func(c *gin.Context) {
-		idStr := c.Param("id")
-		id, err := strconv.ParseInt(idStr, 10, 64)
-		if err != nil {
-			c.JSON(http.StatusBadRequest, "id is not a number")
-			return
-		}
-		err = database.DeleteAuthorById(db, id)
-		if err != nil {
-			c.JSON(http.StatusNotFound, "not found")
-			return
-		}
-		c.Status(http.StatusOK)
-	}
-}
-
-func createPost(db *sqlx.DB) gin.HandlerFunc {
-	return func(c *gin.Context) {
-		title := c.Query("title")
-		content := c.Query("content")
-		newPost := database.NewPost{
-			Title:            title,
-			Content:          content,
-			PublishTimestamp: time.Now(),
-		}
-
-		postId, err := database.AddPost(db, newPost)
-		if err != nil {
-			log.Fatalln(err)
-		}
-		c.JSON(http.StatusOK, postId)
-	}
-}
-
-func getAllAuthors(db *sqlx.DB) gin.HandlerFunc {
-	return func(c *gin.Context) {
-		authors, err := database.GetAllAuthors(db)
-		if err != nil {
-			log.Fatalln(err)
-		}
-		c.JSON(http.StatusOK, authors)
-	}
-}
-
-func getAuthor(db *sqlx.DB) gin.HandlerFunc {
-	return func(c *gin.Context) {
-		idStr := c.Param("id")
-		id, err := strconv.ParseInt(idStr, 10, 64)
-		if err != nil {
-			c.JSON(http.StatusBadRequest, "id is not a number")
-			return
-		}
-		author, err := database.GetAuthorById(db, id)
-		if err != nil {
-			c.JSON(http.StatusNotFound, "not found")
-			return
-		}
-		c.JSON(http.StatusOK, author)
-	}
-}
-
-func createAuthor(db *sqlx.DB) gin.HandlerFunc {
-	return func(c *gin.Context) {
-		name := c.Query("name")
-		surname := c.Query("surname")
-		website := c.Query("website")
-		status := c.Query("status")
-		NewAuthor := database.NewAuthor{
-			Name:    name,
-			Surname: surname,
-			Website: website,
-			Status:  status,
-		}
-
-		authorId, err := database.AddAuthor(db, NewAuthor)
-		if err != nil {
-			log.Fatalln(err)
-		}
-		c.JSON(http.StatusOK, authorId)
-	}
-}
 
 func Serve(db *sqlx.DB) {
 	router := gin.Default()
 
-	postsRoutes := router.Group("/posts")
-	{
-		postsRoutes.GET("/", getAllPosts(db))
-		postsRoutes.POST("/", createPost(db))
-		postsRoutes.GET("/:id", getPost(db))
-		postsRoutes.DELETE("/:id", deletePost(db))
-	}
-
-	authorsRoutes := router.Group("/authors")
-	{
-		authorsRoutes.GET("/", getAllAuthors(db))
-		authorsRoutes.POST("/", createAuthor(db))
-		authorsRoutes.GET("/:id", getAuthor(db))
-		authorsRoutes.DELETE("/:id", deleteAuthor(db))
-	}
+	routes.SetupPostsRoutes(router, db)
+	routes.SetupAuthorsRoutes(router, db)
 
 	router.Run()
 }
